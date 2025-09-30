5b2ff056fa627da4d2e3b9b815e85d3d0aa6e98d
dotSamo <samlilly.p@gmail.com>
2025-09-29 20:56:19 -0800

    Added wires
    Ability to wire components. Visual only

--- Components/Resistor/resistor.gd
+++ Components/Resistor/resistor.gd
@@ -2,13 +2,27 @@ extends Node2D
 
 ## Stores whether the mouse is currently hovering over the component
 var hover = false
+var id: int
+var port = {}
+static var counter = 0
+signal port_clicked(component_id, port_name, position)
 
 func _on_container_mouse_entered():
 	hover = true
 
+
+func _on_container_mouse_exited():
+	if not Input.is_action_pressed("Drag"):
+		hover = false
+
 func _process(_delta):
 	handle_movement()
 
+func _ready():
+	id = counter
+	counter += 1 
+	port["start"] = $Start
+	port["end"] = $End
 
 func handle_movement():
 	if hover and Input.is_action_pressed("Drag"):
@@ -18,6 +32,13 @@ func handle_movement():
 	if hover and Input.is_action_just_pressed("Rotate Right"):
 		rotation_degrees += 45
 
-func _on_container_mouse_exited():
-	if not Input.is_action_pressed("Drag"):
-		hover = false
+
+func _on_start_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		emit_signal("port_clicked", id, "start", %StartPos.global_position)
+		print("Port start clicked on resistor ", id)
+
+func _on_end_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		emit_signal("port_clicked", id, "end", %EndPos.global_position)
+		print("Port end clicked on resistor ", id)
