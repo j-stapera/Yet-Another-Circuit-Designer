f245ed61fe77d5bc757b7fcfe185d29c6208c887
j-stapera <raventhegray@yahoo.com>
2025-12-07 15:42:32 -0900

    removed all files that no longer need to be tracked

--- grid.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-const grid_size = 128
-const grid_color = Color(0.2, 0.2, 0.2, 0.5)
-var wire_mode = false
-var placing_wire = false
-signal theme_change(newValue: bool)
-var currentTheme = false
